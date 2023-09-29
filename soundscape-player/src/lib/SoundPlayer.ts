c234a6102443958fb808693753455c6b85583532
Selim Nahimi <selimnahimi@gmail.com>
2023-09-29 08:13:46 +0000

    Clamp volume between [0.0, 1.0]

--- soundscape-player/src/lib/SoundPlayer.ts
+++ soundscape-player/src/lib/SoundPlayer.ts
@@ -12,10 +12,22 @@ export default class SoundPlayer {
     private static play(file: File, volume: number, loop: boolean = false, game: string): HTMLAudioElement {
         const url = URL.createObjectURL(file);
         const audio = new Audio(url);
-        audio.volume = volume;
+        audio.volume = this.clampVolume(volume);
         audio.loop = loop;
         audio.play();
 
         return audio;
+    }
+
+    private static clampVolume(volume: number): number {
+        if (volume > 1.0) {
+            return 1.0;
+        }
+
+        if (volume < 0.0) {
+            return 0.0;
+        }
+
+        return volume;
     }
 }
